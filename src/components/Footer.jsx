b04873be637fb4c74a9e064903b10466120b6dfc
girum <girumegizachew@gmail.com>
2023-05-27 13:35:01 -0400

    added footbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import { MdNotifications } from "react-icons/md";
-import { RiBrushFill } from "react-icons/ri";
-
-import { FcAbout } from "react-icons/fc";
-import { AiFillSetting } from "react-icons/ai";
-import { BsKeyboard,BsFillQuestionOctagonFill } from "react-icons/bs";
+import { MdOutlineSecurity } from "react-icons/md";
+import { SlNotebook} from "react-icons/sl";
+import { FaCommentsDollar,FaDiscord,FaTwitter } from "react-icons/fa";
+import { AiTwotoneLock,AiOutlineMail } from "react-icons/ai";
+import { BiCodeAlt} from "react-icons/bi";
+RiBrushFill
+import { RiBrushFill} from "react-icons/ri";
 
 function Footer() {
     return (
         <div className="header">
         <div className="header-logo">
             <nav className="navigation">
-                <a href="#" className="footernav-link"> <BsKeyboard/> Contact </a>
-                <a href="#" className="footernav-link"> <BsFillQuestionOctagonFill/> Support </a>
-                <a href="#" className="footernav-link"> <AiFillSetting/> Github </a>
-                <a href="#" className="footernav-link"> <BsKeyboard/> Discord </a>
-                <a href="#" className="footernav-link"> <BsFillQuestionOctagonFill/> Twitter </a>
-                <a href="#" className="footernav-link"> <AiFillSetting/> Terms </a>
-                <a href="#" className="footernav-link"> <BsKeyboard/> Security </a>
-                <a href="#" className="footernav-link"> <BsFillQuestionOctagonFill/> Privacy </a>
+                <a href="#" className="footernav-link"> <AiOutlineMail/> Contact </a>
+                <a href="#" className="footernav-link"> <FaCommentsDollar/> Support </a>
+                <a href="https://github.com/girumgizachew1/type-game" className="footernav-link"> <BiCodeAlt/> Github </a>
+                <a href="#" className="footernav-link"> <SlNotebook/> Terms </a>
+                <a href="#" className="footernav-link"> <MdOutlineSecurity/> Security </a>
+                <a href="#" className="footernav-link"> <AiTwotoneLock/> Privacy </a>
             </nav>
         </div>
         <div className='navigation' >
-            <a href="#" className="footernav-link"><RiBrushFill/> V1.0.0 </a>
+            <a className="footernav-link"><RiBrushFill/> V1.0.0 </a>
         </div>
 
     </div>
